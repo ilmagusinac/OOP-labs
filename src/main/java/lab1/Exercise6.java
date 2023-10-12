61f20cf20048e3521665dd163d0244f312068ab9
ilmagusinac <ilma.gusinac@stu.ibu.edu.ba>
2023-10-12 20:49:54 +0200

    lab1

---
+++ src/main/java/lab1/Exercise6.java
@@ -0,0 +1,23 @@
+package lab1;
+
+import java.util.Scanner;
+
+public class Exercise6 {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Type the first number: ");
+        int number1 = Integer.parseInt(reader.nextLine());
+
+        System.out.print("Type the second number: ");
+        int number2 = Integer.parseInt(reader.nextLine());
+
+        if(number1>number2){
+            System.out.println("Greater number: " + number1);
+        } else if(number1<number2){
+            System.out.println("Greater number: " + number2);
+        } else{
+            System.out.println("The numbers are equal!");
+        }
+    }
+}
